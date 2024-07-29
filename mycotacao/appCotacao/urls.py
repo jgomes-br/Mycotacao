2d917e57eeb9f35fd3636099c45bc019d8fd3478
jgomes-br <joao.09.05@gmail.com>
2024-07-29 00:13:25 -0300

    painel gerenciar

--- mycotacao/appCotacao/urls.py
+++ mycotacao/appCotacao/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.lista_cotacao, name="index"),
     path('cotacao/<int:cod_cotacao>', views.cotacao, name="cotacao"),
+    path('gerenciar/<int:pk>', views.GerenciarProjeto.as_view(), name="gerenciar_projeto"),
     path('cotacaoadm/<int:cod_cotacao>/<int:forn_id>', views.cotacao, name="cotacaoadm"),
 ]
--- mycotacao/appCotacao/views.py
+++ mycotacao/appCotacao/views.py
@@ -1,15 +1,14 @@
+from typing import Any
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-from django.db import IntegrityError
+from django.views.generic.detail import DetailView
 # from django.contrib.auth.models import  User
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Projeto, Fornecedor, Lance, Produto
-from .formularios import TesteForm
-
+from .models import Projeto, Fornecedor, Lance
 # camada logica
 from .opdb import gravar_resposta_form
 
@@ -17,14 +16,61 @@ from .opdb import gravar_resposta_form
 
 @login_required(login_url='/accounts/login/')
 def lista_cotacao(request):
-    # user_id = request.user
-    # fornecedor = Fornecedor.objects.get(dono=user_id)
     lista = Projeto.objects.all()
     pagina = 'appCotacao/lista-projetos-fornecedores.html'
-    if request.user.is_superuser:
-        pagina = 'appCotacao/lista-projetos-manager.html' 
+    return render(request, pagina, context={'projetos': lista, "eu":request.user})
+
+
+# @login_required(login_url='/accounts/login/')
+
+class GerenciarProjeto(DetailView):
+    model = Projeto
+
+    def colunas(self):
+        return ('fornecedor1', 'fornecedor2', 'fornecedor3')
+
+    def tabela(self):
+        temp = {
+            'colunas':('2:fornecedor1', '1:forncedor2', '3:fornecedor3'),
+            'linhas':(
+                ['produto1', [('preco1', 'dono'), 'preco2', 'preco3']],
+                ['produto2', ['preco1', 'preco2', 'preco3']],
+                ['produto3', ['preco1', 'preco2', 'preco3']],
+                )
+        }
+        temp = {'colunas':[], 'linhas':[]}
+        proj = self.get_object()
+
+        for forn in proj.fornecedor.all(): # type: ignore
+            temp['colunas'].append(forn)
+
+        for prod in proj.produto.all(): # type: ignore
+            lista_p = []
+            for forn in temp['colunas']:
+                est = proj.estrutura_set.get(fornecedor__pk=forn.id,  # type: ignore
+                                             produto__descricao=prod.descricao)
+                lance =  est.lance_set.last()
+                lista_p.append((lance.preco, int(est.status), est.id))
+
+            temp['linhas'].append([prod.descricao, lista_p])
+
+
+        return temp
     
-    return render(request, pagina, context={'projetos': lista})
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        contexto =  super().get_context_data(**kwargs)
+        contexto['tabela'] = self.tabela()
+        contexto['eu'] = self.request.user
+        
+        return contexto
+
+# def gerenciar(request, projeto_id):
+#     pagina = 'appCotacao/gerenciar-cotacao.html'
+#     projeto = Projeto.objects.get(pk=projeto_id)
+#     return render(request, pagina, context={"eu":request.user, 
+#                                             'projeto':projeto})
+
+
 
 def cotacao(request, cod_cotacao, forn_id=None):
 
