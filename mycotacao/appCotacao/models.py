267147002bbddab7b248c40119306bf4d7004eef
jgomes-br <joao.09.05@gmail.com>
2024-08-05 00:09:50 -0300

    versao20

--- mycotacao/appCotacao/models.py
+++ mycotacao/appCotacao/models.py
@@ -10,8 +10,8 @@ class Produto(models.Model):
         return self.descricao
     
 class Fornecedor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     nomeempresa = models.CharField(max_length=100, unique=True)
-    responsavel = models.ForeignKey(User,verbose_name = 'User', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.nomeempresa
@@ -27,11 +27,25 @@ class Projeto(models.Model):
     def __str__(self) -> str:
         return self.nome
 
+class Lance(models.Model):
+    OPCOES_STATUS =(
+        ('P', 'Pendente'),
+        ('A', 'Aceito'),
+        ('R', 'Recusado'),
+        ('S', 'Avançou sem resposta')
+    )
+    dono = models.ForeignKey(User,verbose_name = 'User', on_delete=models.CASCADE)
+    lance = models.SmallIntegerField()
+    preco = models.DecimalField(default=Decimal("0.0"), max_digits=5 ,decimal_places=2)
+    status = models.CharField(max_length=1, null=True, choices=OPCOES_STATUS)
+    obs = models.CharField(max_length=100, null=True)
+
+
 class Estrutura(models.Model):
     OPCOES_STATUS =(
         ('0', 'start'),
         ('1', 'fornecedor'),
-        ('2', 'admim'),
+        ('2', 'admin'),
         ('3', 'Finalizado'),
         ('4', 'Nao Pode mais dar Lance'),
         ('5', 'Finalizado sem acordo'),
@@ -42,25 +56,9 @@ class Estrutura(models.Model):
     status = models.CharField(max_length=1, null=True, choices=OPCOES_STATUS)
     lance_vencedor = models.SmallIntegerField(null=True)
     volume = models.IntegerField(null=True)
+    lances = models.ManyToManyField(Lance)
     
 
     class Meta:
         unique_together  = [["produto", "fornecedor", "projeto"]]
 
-
-class Lance(models.Model):
-    OPCOES_STATUS =(
-        ('P', 'Pendente'),
-        ('A', 'Aceito'),
-        ('R', 'Recusado'),
-        ('S', 'Avançou sem resposta')
-    )
-    estrutura = models.ForeignKey(Estrutura, on_delete=models.CASCADE)
-    dono = models.ForeignKey(User,verbose_name = 'User', on_delete=models.CASCADE)
-    lance = models.SmallIntegerField()
-    preco = models.DecimalField(default=Decimal("0.0"), max_digits=5 ,decimal_places=2)
-    status = models.CharField(max_length=1, null=True, choices=OPCOES_STATUS)
-    obs = models.CharField(max_length=100, null=True)
-
-    class Meta:
-        unique_together  = [["lance", "estrutura"]]
